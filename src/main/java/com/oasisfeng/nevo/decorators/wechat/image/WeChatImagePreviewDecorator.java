646efc6e261cc93fe61659560e090aa88935f5bd
kr328 <kr328@users.noreply.github.com>
2019-01-09 00:03:43 +0800

    async load

--- src/main/java/com/oasisfeng/nevo/decorators/wechat/image/WeChatImagePreviewDecorator.java
+++ src/main/java/com/oasisfeng/nevo/decorators/wechat/image/WeChatImagePreviewDecorator.java
@@ -1,14 +1,15 @@
 package com.oasisfeng.nevo.decorators.wechat.image;
 
-import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.util.Log;
+//import android.util.Log;
 
 import com.oasisfeng.nevo.sdk.MutableNotification;
 import com.oasisfeng.nevo.sdk.MutableStatusBarNotification;
@@ -17,19 +18,25 @@ import com.oasisfeng.nevo.sdk.NevoDecoratorService;
 import java.io.File;
 import java.util.HashMap;
 
+import androidx.annotation.RequiresApi;
 import androidx.core.content.FileProvider;
 
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.N;
 import static android.os.Build.VERSION_CODES.O;
 import static android.os.Build.VERSION_CODES.P;
 
 /**
  * Created by Oasis on 2018-11-30.
+ * Modify by Kr328 on 2019-1-5
  */
+
 public class WeChatImagePreviewDecorator extends NevoDecoratorService {
-	private static final String NEVO_PACKAGE = "com.oasisfeng.nevo";
+	private static final String NEVO_PACKAGE_NAME = "com.oasisfeng.nevo";
+	private static final String SYSTEM_UI_PACKAGE_NAME = "com.android.systemui";
+	private static final String WECHAT_PACKAGE_NAME = "com.tencent.mm";
 
 	private static final String KEY_DATA_MIME_TYPE = "type";
 	private static final String KEY_DATA_URI= "uri";
@@ -46,40 +53,89 @@ public class WeChatImagePreviewDecorator extends NevoDecoratorService {
 			return;
 		}
 
-		final File image = null;//WeChatImageLoader.loadImage();
-		if (image == null) return;
+		if ((SDK_INT >= N && handleMessagingStyle(evolving.getKey() ,n)) || handleBigPicture(evolving.getKey() ,n) )
+			Log.i(Global.TAG ,"Applied " + evolving.getKey());
+		else
+			Log.w(Global.TAG ,"Unsupported notification " + evolving.getNotification());
+	}
 
-		Log.i(TAG ,"Image Load " + image);
+	@RequiresApi(api = Build.VERSION_CODES.N)
+	private boolean handleMessagingStyle(String key , MutableNotification notification) {
+		Parcelable[] messages = notification.extras.getParcelableArray(Notification.EXTRA_MESSAGES);
+		if ( SDK_INT < P || messages == null || messages.length < 1 ) return false;
+		int index = messages.length - 1;
+		Object last_message = messages[index];
+		if (!(last_message instanceof Bundle)) return false;
+		File image = imageStack.getImageFileForKey(key ,index);
 
-		@SuppressLint("InlinedApi") final Parcelable[] messages = n.extras.getParcelableArray(Notification.EXTRA_MESSAGES);
-		if (SDK_INT >= P && messages != null && messages.length > 0) {
-			final Object last = messages[messages.length - 1];
-			if (! (last instanceof Bundle)) return;
-			final Bundle last_message = (Bundle) last;
-			if (last_message.containsKey(KEY_DATA_MIME_TYPE) && last_message.containsKey(KEY_DATA_URI)) return;
-			Uri uri = FileProvider.getUriForFile(this ,BuildConfig.APPLICATION_ID + ".wechat_root" ,image);
-			Log.i(TAG ,"Uri = " + uri);
-			last_message.putString(KEY_DATA_MIME_TYPE, "image/jpeg");
-			last_message.putParcelable(KEY_DATA_URI, uri);	// TODO: Keep image mapping for previous messages.
-			grantNevoUriReadPermission(evolving.getKey() ,uri);
-			n.extras.putParcelableArray(Notification.EXTRA_MESSAGES, messages.clone()); // Use clone to tell the SDK it is actually changed.
-		} else if (messages == null || messages.length == 1) {
-			final BitmapFactory.Options options = new BitmapFactory.Options();
-			options.inPreferredConfig = SDK_INT >= O ? Bitmap.Config.HARDWARE : Bitmap.Config.ARGB_8888;
-			n.extras.putString(Notification.EXTRA_TEMPLATE, TEMPLATE_BIG_PICTURE);
-			n.extras.putParcelable(Notification.EXTRA_PICTURE, BitmapFactory.decodeFile(image.getPath(), options));
-			n.extras.putCharSequence(Notification.EXTRA_SUMMARY_TEXT, text);
+		if ( image == null ) {
+			imageStack.postLoadImage(key ,index ,notification.when);
 		}
+		else {
+			Uri uri = FileProvider.getUriForFile(this ,
+					BuildConfig.APPLICATION_ID + ".wechat_root" ,
+					image);
+
+			Log.i(Global.TAG ,"Uri = " + uri);
+
+			if ( applyMessageStyle((Bundle) last_message ,uri ,key) )
+				notification.extras.putParcelableArray(Notification.EXTRA_MESSAGES ,messages.clone());
+		}
+
+		return true;
+	}
+
+	private boolean handleBigPicture(String key ,MutableNotification n) {
+		File image = imageStack.getImageFileForKey(key ,0);
+
+		if ( image == null )
+			imageStack.postLoadImage(key ,0 ,n.when);
+		else
+			applyNormal(n ,n.extras.getCharSequence(Notification.EXTRA_TEXT) ,image);
+
+		return true;
+	}
+
+	private boolean applyMessageStyle(Bundle data ,Uri uri ,String key) {
+		//if (data.containsKey(KEY_DATA_MIME_TYPE) && data.containsKey(KEY_DATA_URI)) return false;
+		grantUriReadPermission(key ,uri);
+		data.putString(KEY_DATA_MIME_TYPE, "image/jpeg");
+		data.putParcelable(KEY_DATA_URI, uri);
+		return true;
+	}
+
+	private void applyNormal(MutableNotification n ,CharSequence text ,File image) {
+		final BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inPreferredConfig = SDK_INT >= O ? Bitmap.Config.HARDWARE : Bitmap.Config.ARGB_8888;
+		n.extras.putString(Notification.EXTRA_TEMPLATE, TEMPLATE_BIG_PICTURE);
+		n.extras.putParcelable(Notification.EXTRA_PICTURE, BitmapFactory.decodeFile(image.getPath(), options));
+		n.extras.putCharSequence(Notification.EXTRA_SUMMARY_TEXT, text);
 	}
 
 	@Override
 	protected void onNotificationRemoved(String key, int reason) {
 		revokeUriReadPermission(key);
+		imageStack.removeKey(key);
 	}
 
-	private void grantNevoUriReadPermission(String key , Uri uri) {
+	@Override
+	protected void onConnected() {
+		if ( !imageStack.isAlive() )
+			imageStack.start();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+
+		imageStack.interrupt();
+	}
+
+	private void grantUriReadPermission(String key , Uri uri) {
 		revokeUriReadPermission(key);
-		grantUriPermission(NEVO_PACKAGE ,uri ,Intent.FLAG_GRANT_READ_URI_PERMISSION);
+		grantUriPermission(NEVO_PACKAGE_NAME ,uri ,Intent.FLAG_GRANT_READ_URI_PERMISSION);
+		grantUriPermission(WECHAT_PACKAGE_NAME ,uri ,Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        grantUriPermission(SYSTEM_UI_PACKAGE_NAME ,uri ,Intent.FLAG_GRANT_READ_URI_PERMISSION);
 		grantedUriMap.put(key, uri);
 	}
 
